e2b15c1d8bd3ded253f7efddcecae8e395705bf8
sdonne <stefanodonne@gmail.com>
2021-03-18 10:56:56 +0100

    Validate training of the proto-model

--- test.py
+++ test.py
@@ -1,38 +1,58 @@
-import tensorflow as tf
-import sys
+import os
 import pathlib
-from tensorflow.keras.preprocessing import image_dataset_from_directory
-from tensorflow.keras import layers
+import random
+import sys
+import cv2 as cv
 import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 
-print(tf.__version__)
-
-data_dir = pathlib.Path(str(sys.argv[1]))
-image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)
+study_dir = pathlib.Path(str(sys.argv[1]))
 
 batch_size = 32
-img_height = 180
-img_width = 180
+img_height = 160
+img_width = 160
 
-train_ds = image_dataset_from_directory(
-    data_dir,
+evals_ds = image_dataset_from_directory(
+    study_dir,
+    shuffle=True,
     validation_split=0.1,
     subset="training",
     seed=123,
     image_size=(img_height, img_width),
     batch_size=batch_size)
 
-print(train_ds.class_names)
+model = tf.keras.models.load_model("./model.h5")
+# model trained on kaggle with kvasir dataset
 
-# TEST
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(images[i].numpy().astype("uint8"))
-        plt.title(train_ds.class_names[labels[i]])
-        plt.axis("off")
-plt.show()
-# TEST
-normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+model.summary()
+model.compile(
+    optimizer='adam',
+    loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+model.evaluate(evals_ds)  # evaluate on chu st pierre study set (~95% acc)
+
+class_n = evals_ds.class_names
+while True:
+    k = cv.waitKey(1) & 0xFF
+    if k == ord('q'):
+        break
+    random_dir = random.choice(os.listdir(str(sys.argv[1])))
+    random_source = random.choice(os.listdir(str(sys.argv[1]) + random_dir + "/"))
+    source = cv.imread(str(sys.argv[1]) + random_dir + "/" + random_source)
+    img = source.copy()
+    img = cv.resize(img, (160, 160))
+    img = np.reshape(img, [1, 160, 160, 3])
+    prediction = model.predict(img)
+    print(prediction[0])
+    index = np.argmax(prediction[0])
+    source = cv.resize(source, None, fx=0.5, fy=0.5, interpolation=cv.INTER_CUBIC)
+    source = cv.putText(source, 'Pred : %s , True : %s' % (class_n[index], random_dir), (5, 20),
+                        cv.FONT_HERSHEY_SIMPLEX, 0.7,
+                        (255, 0, 0),
+                        1,
+                        cv.LINE_AA)
+    cv.imshow('proto', source)
+    while (cv.waitKey(1) & 0xFF) != ord('p'):
+        cv.waitKey(1)
